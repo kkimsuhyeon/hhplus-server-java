75f4336c8eefc7f50658c1539e0157ef801e5905
kkimsuhyeon <wzxy2002@naver.com>
2025-09-24 20:24:57 +0900

    lombok 추가, jpa 설정 추
    # Conflicts:
    #	src/main/resources/application.yml

--- build.gradle.kts
+++ build.gradle.kts
@@ -35,6 +35,9 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.projectlombok:lombok")
+    annotationProcessor("org.projectlombok:lombok")
+    testImplementation("org.projectlombok:lombok")
+    testAnnotationProcessor("org.projectlombok:lombok")
 
     // DB
     runtimeOnly("com.mysql:mysql-connector-j")
